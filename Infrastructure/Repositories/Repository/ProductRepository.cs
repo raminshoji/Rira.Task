54a109c3c56f5051e4f562f20de8b04cdd7139e2
Ramin Shojaei <starsanarin@gmail.com>
2025-04-16 16:33:51 +0330

    Constructor for domain

--- Infrastructure/Repositories/Repository/ProductRepository.cs
+++ Infrastructure/Repositories/Repository/ProductRepository.cs
@@ -10,15 +10,20 @@ using System.Threading.Tasks;
 namespace Infrastructure.Repositories.Repository
 {
     public class ProductRepository : IProductRepository
+        
     {
-        private readonly List<Product> _products =
-        [
+        private readonly List<Product> _products;
+        public ProductRepository()
+        {
+            _products = new List<Product>
+        {
             new Product { Id = 1, Name = "Product A", Category = Categories.Category1, Price = 100 },
             new Product { Id = 2, Name = "Product B", Category = Categories.Category1, Price = 150 },
             new Product { Id = 3, Name = "Product C", Category = Categories.Category2, Price = 120 },
             new Product { Id = 4, Name = "Product D", Category = Categories.Category3, Price = 200 },
             new Product { Id = 5, Name = "Product E", Category = Categories.Category1, Price = 80 }
-        ];
+        };
+        }
         public List<Product> GetAllProducts()
         {
             return _products;
