c363c5891a638a735e618f6a0283bab00a018d23
Ramin Shojaei <starsanarin@gmail.com>
2025-04-16 16:44:51 +0330

    Create Constructor

--- Infrastructure/Repositories/Repository/ProductRepository.cs
+++ Infrastructure/Repositories/Repository/ProductRepository.cs
@@ -15,14 +15,14 @@ namespace Infrastructure.Repositories.Repository
         private readonly List<Product> _products;
         public ProductRepository()
         {
-            _products = new List<Product>
-        {
+            _products =
+        [
             new Product { Id = 1, Name = "Product A", Category = Categories.Category1, Price = 100 },
             new Product { Id = 2, Name = "Product B", Category = Categories.Category1, Price = 150 },
             new Product { Id = 3, Name = "Product C", Category = Categories.Category2, Price = 120 },
             new Product { Id = 4, Name = "Product D", Category = Categories.Category3, Price = 200 },
             new Product { Id = 5, Name = "Product E", Category = Categories.Category1, Price = 80 }
-        };
+        ];
         }
         public List<Product> GetAllProducts()
         {
